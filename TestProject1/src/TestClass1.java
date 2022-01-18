417cde93fd8dee35bd83ce9d6fe257624cf7d90d
neeth <neeth@192.168.1.212>
2022-01-18 15:40:47 -0600

    Second commit after adding some variables

--- TestProject1/src/TestClass1.java
+++ TestProject1/src/TestClass1.java
@@ -2,7 +2,11 @@
 public class TestClass1 {
 
 	public static void main(String[] args) {
+		int a=1,b=2;
+		int sum=0;
+		sum= a+b;
 		System.out.println("Hello");
-	}
+		System.out.println(sum);
+		}
 
 }
