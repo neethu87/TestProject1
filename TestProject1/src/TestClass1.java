4097ff47590ee169ebfae95a030700481a4e503d
Neethu Kuriakose <neethukuriakose3@gmail.com>
2022-01-19 10:30:19 -0600

    Added d

--- TestProject1/src/TestClass1.java
+++ TestProject1/src/TestClass1.java
@@ -5,10 +5,10 @@ public class TestClass1 {
 		int a=1,b=2;
 		int sum=0;
 		sum= a+b;
-		int c=5;
-		c= c+b;
+		int d =0;
+		d= d+b;
 		System.out.println("Hello");
-		System.out.println(c);
+		System.out.println(d);
 		System.out.println(sum);
 		System.out.println("Added this line from GitHub to learn Fetch from Upstream and get this new changes to Local");
 		}
