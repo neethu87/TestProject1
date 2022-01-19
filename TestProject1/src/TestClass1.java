ce1bfc864bc10c87faca6c0739a0b635dc0f828c
Neethu Kuriakose <neethukuriakose3@gmail.com>
2022-01-19 10:07:14 -0600

    Added new field name

--- TestProject1/src/TestClass1.java
+++ TestProject1/src/TestClass1.java
@@ -5,7 +5,8 @@ public class TestClass1 {
 		int a=1,b=2;
 		int sum=0;
 		sum= a+b;
-		System.out.println("Hello");
+		String name = "friend";
+		System.out.println("Hello" + name);
 		System.out.println(sum);
 		System.out.println("Added this line from GitHub to learn Fetch from Upstream and get this new changes to Local");
 		}
